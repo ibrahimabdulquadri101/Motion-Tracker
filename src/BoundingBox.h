99fa4df450c0bf37e896a8e527c58d2c4c261cd2
ibrahim <ibrahimabdulquadri446@gmail.com>
2026-09-04 18:54:07 +0100

    FC.h added

--- src/BoundingBox.h
+++ src/BoundingBox.h
@@ -1,4 +1,14 @@
 #ifndef BOUNDINGBOX_H
 #define BOUNDINGBOX_H
 
+struct Boundingbox
+{
+    int width;
+    int height;
+    int x;
+    int y;
+    float confidence;
+    int id;
+};
+
 #endif // BOUNDINGBOX_H
--- src/FrameCapture.h
+++ src/FrameCapture.h
@@ -1,4 +1,16 @@
 #ifndef FRAMECAPTURE_H
 #define FRAMECAPTURE_H
+#include "opencv2/opencv.hpp"
+using namespace cv;
+
+class FrameCapture
+{
+    private:
+        cv::VideoCapture cap;
+        int frameWidth;
+        int frameHeight;
+        double fps;
+        bool isOpen;
+};
 
 #endif // FRAMECAPTURE_H
