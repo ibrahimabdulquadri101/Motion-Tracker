083484fe7178e28dc75f73ddc213eb1868562210
ibrahim <ibrahimabdulquadri446@gmail.com>
2026-09-05 15:03:27 +0100

    add a background-model

--- src/BackgroundModel.cpp
+++ src/BackgroundModel.cpp
@@ -1,6 +1,7 @@
 #include "BackgroundModel.h"
 #include <opencv2/opencv.hpp>
-
+#include <cmath>
+#include "FrameCapture.h"
 void BackgroundModel::init(cv::Mat& firstFrame, float learningRate, int threshold)
 {
     cv::Mat gray;
@@ -10,5 +11,28 @@ void BackgroundModel::init(cv::Mat& firstFrame, float learningRate, int threshol
     this->learningRate = learningRate;
     this->threshold = threshold;
     this->initialized = true;
-    
+}
+
+cv::Mat BackgroundModel::apply(cv::Mat& frame)
+{
+    cv::Mat gray;
+    cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+
+    cv::Mat gray32;
+    gray.convertTo(gray32, CV_32F);
+
+    cv::Mat absDiff;
+    cv::absdiff(background, gray32, absDiff);
+
+    cv::Mat mask;
+    cv::threshold(absDiff, mask, this->threshold, 255, cv::THRESH_BINARY);
+    mask.convertTo(mask, CV_8U);
+    cv::accumulateWeighted(gray,background,this -> learningRate);
+    return mask;
+}
+
+void BackgroundModel::reset()
+{
+    background.release();
+    initialized = false;
 }
